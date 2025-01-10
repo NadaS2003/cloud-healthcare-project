b037107f1713fafb99059341bbd4b5f4384d41f2
Nana <nadaramadan1512003@gmail.com>
2025-01-10 11:17:53 +0200

    update Configurations files app

---
+++ database/migrations/2025_01_07_120236_add_diagnosis_id_to_invoices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->unsignedBigInteger('diagnosis_id')->nullable()->after('patient_id'); // التأكد من أنه بعد patient_id
+            $table->foreign('diagnosis_id')->references('id')->on('diagnoses')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropForeign(['diagnosis_id']);
+            $table->dropColumn('diagnosis_id');
+        });
+    }
+};
